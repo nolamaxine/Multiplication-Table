588249d077582e29c069a02303fb5c0256d6b2fe
nolapatneaude <nolapatneaude@gmail.com>
2025-10-01 15:12:06 -0500

    Added code from Tina from last class

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-alert("Hello World! :)")
+alert("Math time :(")
 console.log('Here\'s a for loop showing 1...10:\n')
 for (let i =0; i < 10; i++) {
     console.log(`${i}\n`)
@@ -44,3 +44,41 @@ let timesTable= (to) => {
 timesTable (4)
 timesTable (12)
 timesTable ()
+// code from Tina
+function getRowsDesired() {
+    // console.log(Number(document.querySelector('.rows input#rows').value))
+    // console.log(Number(document.querySelectorAll('.rows input')[0].value))
+    console.log(Number(document.getElementById('rows').value))
+    return Number(document.getElementById('rows').value);
+}
+
+function getColumnsDesired() {
+    console.log(Number(document.getElementById('cols').value))
+    return Number(document.getElementById('cols').value)
+}
+
+
+function createWithHtmlTable(rows, cols) {
+    let string
+    if (rows && cols) {
+        string = "<table>\n"
+
+        for (let row = 1; row <= rows; row++) {
+            string += `<tr>\n`
+
+            for (let col = 1; col <= cols; col++) {
+                string += "<td>"
+                string += row * col
+                string += "<td>\n"
+            }
+            string += "</tr>\n"
+        }
+
+        string += "</table>\n"
+    } else {
+        string = 'Provide some inputs'
+    }
+    document.getElementById("output").innerHTML = string;
+
+}
+
