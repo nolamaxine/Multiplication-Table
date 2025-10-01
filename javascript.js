7fc500583328b513c75590871c6a9e7a1bdd2133
nolapatneaude <nolapatneaude@gmail.com>
2025-10-01 16:56:50 -0500

    added highlight functionality

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
-alert("Math time :(")
+// alert("Math time :(")
 console.log('Here\'s a for loop showing 1...10:\n')
-for (let i =0; i < 10; i++) {
+for (let i = 0; i < 10; i++) {
     console.log(`${i}\n`)
 }
 
@@ -21,29 +21,30 @@ while (j < 10) {
 console.log('Here\'s a foreach loop iterating over an array showing 1...10:\n')
 let nums = [...Array(10).keys()]
 nums.forEach(num => {
-    console.log(`${ num }\n`)
+    console.log(`${num}\n`)
 })
 
 console.log('Here\'s a multiplication table method rendered in the console with a for loop:\n')
-let timesTable= (to) => {
-    console.log('Here\'s a times table to ' + to + '\n' )
+let timesTable = (to) => {
+    console.log('Here\'s a times table to ' + to + '\n')
 
     if (Number.isInteger(to)) {
-        for (let i =1; i <= to; i++) {
+        for (let i = 1; i <= to; i++) {
             let row = ""
-            for (let j =1; j<= to; j++) {
-                row += `${ i * j }\t`
+            for (let j = 1; j <= to; j++) {
+                row += `${i * j}\t`
             }
             console.log(row)
         }
-    }else{
+    } else {
         console.log(`Sorry, ${to} isn't a number!`)
     }
 }
 
-timesTable (4)
-timesTable (12)
-timesTable ()
+timesTable(4)
+timesTable(12)
+timesTable()
+
 // code from Tina
 function getRowsDesired() {
     // console.log(Number(document.querySelector('.rows input#rows').value))
@@ -57,25 +58,39 @@ function getColumnsDesired() {
     return Number(document.getElementById('cols').value)
 }
 
+function getHighlightValue() {
+    let highlightValue = document.getElementById("highlight").value;
 
-function createWithHtmlTable(rows, cols) {
+    if (highlightValue !== '') {
+        return Number(highlightValue)
+    }
+}
+
+
+function createWithHtmlTable(rows, cols, highlight) {
     let string //define a variable called string
     if (rows && cols) { //if rows nd columns were provided
         string = "<table>\n" //start building the HTML table
 
         for (let row = 1; row <= rows; row++) {
-            string += `<tr>\n` //start building HTML row
+
+            if (highlight && highlight === row) {
+                string += "<tr class='highlight'>\n"
+            } else {
+                string += `<tr>\n` //start building HTML row
+            }
+
 
             for (let col = 1; col <= cols; col++) {
                 string += "<td>" //start building a cell
                 string += row * col //populate contents of cell
-                string += "<td>\n" //complete cell
+                string += "</td>\n" //complete cell
             }
             string += "</tr>\n" //complete HTML row
         }
 
         string += "</table>\n"
-    } else { //method wasn't provided
+    } else { //either "rows" or "cols" wasn't provided
         string = 'Provide some inputs' //text "provide some inputs" will show
     }
     document.getElementById("output").innerHTML = string;
