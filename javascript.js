912cba94e605a90fe810671f1756073b2e06b035
nolapatneaude <nolapatneaude@gmail.com>
2025-10-15 16:09:50 -0500

    Matched to Nik's Repo pt 2

--- javascript.js
+++ javascript.js
@@ -87,6 +87,40 @@
 //     }
 // }
 
+// function getRowsDesired() {
+//     return parseInt(document.getElementById("rows").value)
+// }
+//
+// function getColumnsDesired() {
+//     return parseInt(document.getElementById("cols").value)
+// }
+//
+// function createWithFlexbox(rows, cols) {
+//     let string = ''
+//
+//     // first do-while loop to build the flex'd rows
+//     let row = 1
+//     do {
+//         string += "<div class='row'>"
+//         string += row
+//
+//         // second do-while loop to build the flex'd columns
+//         let col = 1
+//         do {
+//             string += "<div class='col'>"
+//             string += row * col
+//             string += "</div>"
+//
+//             col = col + 1
+//         } while (col <= cols)
+//         string += "</div>\n"
+//
+//         row = row + 1;
+//     } while (row <= rows)
+//
+//     document.getElementById("output").innerHTML = string
+// }
+
 function getRowsDesired() {
     return parseInt(document.getElementById("rows").value)
 }
